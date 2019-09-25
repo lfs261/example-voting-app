dd74858e24795754671892128223dc2d0fb1e32b
LMoreno <luis.moreno@stoneridge.com>
2019-09-25 09:03:40 -0600

    Added a unit test

--- result/test/mock.test.js
+++ result/test/mock.test.js
@@ -30,4 +30,10 @@ describe('mock test 5', () => {
   it('unit test 5', () => {
     expect(true).to.be.true;
   });
+
+describe('mock test 6', () => {
+  it('unit test 6', () => {
+    expect(true).to.be.true;
+  });
+
 });
