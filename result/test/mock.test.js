5f51fe179ac8a30f2fa8e53b6c36b3b149e9071f
mgaynutdinov <m.gaynutdinov@mail.ru>
2020-12-18 13:28:58 +0100

    new unitworker test

--- result/test/mock.test.js
+++ result/test/mock.test.js
@@ -32,10 +32,15 @@ describe('mock test 4', () => {
   });
 });
 
-describe('mock test 4', () => {
+describe('mock test 5', () => {
   it('unit test 4', () => {
     expect(true).to.be.true;
   });
+describe('mock test 6', () => {
+  it('unit test 4', () => {
+    expect(true).to.be.true;
+  });
+
 });
 
 
